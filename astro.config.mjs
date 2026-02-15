fcb584a473dd701c1faf5be3a7b58437c9af11ec
flexpertsdev <155709656+flexpertsdev@users.noreply.github.com>
2026-02-15 15:18:40 +0000

    fix: auto-fix build errors (2 files)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ckbfirm.com',
   integrations: [
-tailwind({
+    tailwind({
       applyBaseStyles: false,
-    }), sitemap()]
+    }),
+  ]
 });
